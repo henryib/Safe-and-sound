dd41bf6ec94da97600bfa0ad5aab3837e35fe785
ryanchoiGH <78860277+ryanchoiGH@users.noreply.github.com>
2022-04-10 23:31:14 +0100

    Updated Venues
    Added Venues ID and deactivate/activate button

--- venues.js
+++ venues.js
@@ -24,3 +24,13 @@ modeSwitch.addEventListener("click" , () =>{
         
     }
 });
+
+function activateButton(button) {
+    if (button.innerHTML === "Deactivate") {
+      button.innerHTML = "Activate";
+      button.style.backgroundColor = "green";
+    } else {
+      button.innerHTML = "Deactivate";
+      button.style.backgroundColor = "red";
+    }
+  }
