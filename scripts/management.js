d82a21649b781aa1914ec1aa5531d486068495da
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-13 01:03:25 +0100

    show spinner

--- scripts/management.js
+++ scripts/management.js
@@ -3,5 +3,6 @@ function getAllPolice() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("loading").style.display = "block";
     let police = getAllPolice()
 });
