0f6de7baf24afb19285badb3dc110b8dc2e1c497
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-13 17:52:40 +0100

    table css

--- scripts/venues.js
+++
@@ -1,9 +0,0 @@
-function activateButton(button) {
-    if (button.innerHTML === "Deactivate") {
-        button.innerHTML = "Activate";
-        button.style.backgroundColor = "green";
-    } else {
-        button.innerHTML = "Deactivate";
-        button.style.backgroundColor = "red";
-    }
-}
