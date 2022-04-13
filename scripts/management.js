da8cb822ea2121948788921b00520d940b386c5b
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-13 01:07:24 +0100

    hide spinner on getting police list

--- scripts/management.js
+++ scripts/management.js
@@ -1,8 +1,26 @@
-function getAllPolice() {
+function getAllPolice(jwt) {
+    fetch('https://safe-sound-208.herokuapp.com/police', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`
+        }
+    })
+        .then((response) => response.json())
+        .then((data) => handleResponse(data))
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
+function handleResponse(data) {
+    document.getElementById("loading").style.display = "none";
+    console.log(data);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("loading").style.display = "block";
-    let police = getAllPolice()
+    let jwt = localStorage.getItem("jwt");
+    let police = getAllPolice(jwt)
 });
