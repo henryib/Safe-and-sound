7321935ef83fbce87846d68df89846c3c7070149
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-18 19:09:10 +0100

    with row

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = new WebSocket('wss://safe-sound-208.herokuapp.com/reports/add/police');
 
+
     socket.addEventListener('message', function (event) {
-        console.log('Message from server ', event.data.split(" "));
+        console.log('Message from server ', event);
+        console.log('Message from server ', event.data);
+        let report = event.data.generic;
+        let table_body = document.getElementById("live_table");
+        let row = '<tr><td></td></tr>'
     });
 
 });
