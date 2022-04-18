cdaaa0526f5a74b4e4c2f3424a35605e74835d03
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-19 00:21:07 +0100

    bug fix on indexes

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const socket = new WebSocket('wss://safe-sound-208.herokuapp.com/reports/add/police');
 
     socket.addEventListener('message', function (event) {
-        i++;
         let data = JSON.parse(event.data);
         let report = data.generic;
         reports.push(report);
@@ -26,5 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
         let table_body = document.getElementById("live_table");
         let row = `<tr><td>${report.report_id}</td><td>${report.report_user}</td><td>${report.report_phone}</td><td>${date}</td><td>${report.report_type}</td><td>${report.report_venue}</td><td><button onclick="showDetail(this)" class="more" id="${i}"><i class='bx bx-detail'></i></button></td></tr>`;
         table_body.innerHTML += row;
+        i++;
     });
 });
