48d848851554d4fa6e04f648ee5b346760c0922c
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-18 19:50:15 +0100

    with storage of reports

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = new WebSocket('wss://safe-sound-208.herokuapp.com/reports/add/police');
-
+    let reports = [];
+    let i = 0;
 
     socket.addEventListener('message', function (event) {
+        i++;
         let data = JSON.parse(event.data);
         let report = data.generic;
+        reports.push(report);
         let date = report.report_date.replace("T", " ");
         let table_body = document.getElementById("live_table");
-        let row = `<tr><td>${report.report_user}</td><td>${report.report_phone}</td><td>${date}</td><td>${report.report_type}</td><td>${report.report_venue}</td><td><button class="more" id="${report.report_id}"><i class='bx bx-detail'></i></button></td></tr>`;
+        let row = `<tr><td>${report.report_user}</td><td>${report.report_phone}</td><td>${date}</td><td>${report.report_type}</td><td>${report.report_venue}</td><td><button class="more" id="${i}"><i class='bx bx-detail'></i></button></td></tr>`;
         table_body.innerHTML += row;
     });
 
