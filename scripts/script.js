b677f66b984358910258cbfd8dc5e7b77f0bd002
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-18 19:30:39 +0100

    data

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     socket.addEventListener('message', function (event) {
         let report = event.data.generic;
-        console.log(event.data.generic)
+        console.log(event.data)
         let table_body = document.getElementById("live_table");
         let row = '<tr><td></td></tr>'
     });
