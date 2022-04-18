64ada2d36dd72d7e2abc5fb89407676280b5a9e0
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-18 16:39:35 +0100

    removed dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const socket = new WebSocket('wss://safe-sound-208.herokuapp.com/reports/add/police');
 
     socket.addEventListener('message', function (event) {
-        console.log('Message from server ', event.data);
+        console.log('Message from server ', event.data.split(" "));
     });
 
 });
