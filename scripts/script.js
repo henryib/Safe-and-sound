bfef6f9daec8a040d0cf297a73105b030c9decb4
João Garrido <joao.melo.garrido@hotmail.com>
2022-04-18 16:30:26 +0100

    websocket connected

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const socket = new WebSocket('wss://safe-sound-208.herokuapp.com/reports/add/police');
 
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+    });
 
 });
